d40d5a9ba2183fd741d90d171afd2c75930ccb0f
Pratik Fuse <pratikfuse20@gmail.com>
2023-09-29 15:18:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let line = document.getElementById("line");
+
+let deg = 0;
+setInterval(rotateBy20Deg, 20);
+
+function rotateBy20Deg() {
+  console.log("rotate");
+  line.style.transform = `rotate(${(deg += 20)}deg)`;
+}
